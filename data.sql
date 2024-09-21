8b952d6d4b3d34e7c39b5a932e0fcf23799baa2f
Saurabh Kumar <skdubey568@gmail.com>
2024-09-21 19:12:57 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE india_post_db;
+
+USE india_post_db;
+
+CREATE TABLE parcel_deliveries (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_name VARCHAR(255) NOT NULL,
+    recipient_name VARCHAR(255) NOT NULL,
+    sender_contact VARCHAR(20) NOT NULL,
+    recipient_contact VARCHAR(20) NOT NULL,
+    delivery_address TEXT NOT NULL,
+    delivery_date DATE NOT NULL,
+    parcel_weight FLOAT NOT NULL,
+    weight_unit ENUM('g', 'kg') NOT NULL,
+    time_slot VARCHAR(50) NOT NULL
+);
