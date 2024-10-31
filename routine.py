c40db21556327e6dda0af48845d19aea9d0d8ec9
Saurabh Kumar <skdubey568@gmail.com>
2024-10-31 12:14:14 +0530

    Update routine.py

--- routine.py
+++ routine.py
@@ -14,12 +14,10 @@ app.config['MYSQL_DB'] = 'india_post_db'
 
 mysql = MySQL(app)
 
-# Serve the index.html file from the static folder as default
 @app.route('/')
 def index():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'index.html')
 
-# Route to fetch records based on id, pincode, and delivery date
 @app.route('/get_postman', methods=['POST'])
 def get_postman():
     data = request.get_json()
@@ -32,41 +30,62 @@ def get_postman():
 
     cur = mysql.connection.cursor()
 
-    # Query to fetch Postman details based on id and pincode
-    cur.execute("SELECT * FROM Postman WHERE id = %s AND pincode = %s", (postman_id, pincode))
+    # Query Postman details
+    cur.execute("SELECT id, name, phone_number, pincode FROM Postman WHERE id = %s AND pincode = %s", (postman_id, pincode))
     postman_result = cur.fetchone()
 
     if not postman_result:
         cur.close()
         return jsonify({'message': 'No records found for the given ID and Pincode'}), 404
 
-    # Query to fetch parcel deliveries based on the entered pincode and delivery date
-    cur.execute("""
-        SELECT recipient_name, recipient_contact, delivery_address, delivery_date, time_slot 
+    # Query deliveries
+    cur.execute(""" 
+        SELECT id, recipient_name, recipient_contact, recipient_address, delivery_date, delivery_time_slot 
         FROM parcel_deliveries 
-        WHERE delivery_pincode = %s AND delivery_date = %s
+        WHERE recipient_pincode = %s AND delivery_date = %s
     """, (pincode, delivery_date))
     delivery_result = cur.fetchall()
+
+    # Query pickups
+    cur.execute(""" 
+        SELECT id, sender_name, sender_contact, sender_address, pickup_date, pickup_time_slot 
+        FROM parcel_deliveries
+        WHERE sender_pincode = %s AND pickup_date = %s
+    """, (pincode, delivery_date))
+    pickup_result = cur.fetchall()
+
     cur.close()
 
-    deliveries = []
-    for delivery in delivery_result:
-        deliveries.append({
-            'recipient_name': delivery[0],
-            'recipient_contact': delivery[1],
-            'delivery_address': delivery[2],
-            'delivery_date': delivery[3],
-            'time_slot': delivery[4]
-        })
+    # Prepare deliveries data
+    deliveries = [{
+        'id': delivery[0],
+        'recipient_name': delivery[1],
+        'recipient_contact': delivery[2],
+        'delivery_address': delivery[3],
+        'delivery_date': delivery[4],
+        'delivery_time_slot': delivery[5]
+    } for delivery in delivery_result]
 
-    return jsonify({
-        'postman': {
-            'id': postman_result[0],
-            'name': postman_result[1],
-            'pincode': postman_result[2]
-        },
-        'deliveries': deliveries
-    }), 200
+    # Prepare pickups data
+    pickups = [{
+        'id': pickup[0],
+        'sender_name': pickup[1],
+        'sender_contact': pickup[2],
+        'sender_address': pickup[3],
+        'pickup_date': pickup[4],
+        'pickup_time_slot': pickup[5]
+    } for pickup in pickup_result]
+
+    # Prepare postman data
+    postman_data = {
+        'id': postman_result[0],
+        'name': postman_result[1],
+        'phone_number': postman_result[2],
+        'pincode': postman_result[3],
+        'profile_image': None  # Add logic for postman profile image if necessary
+    }
+
+    return jsonify({'postman': postman_data, 'deliveries': deliveries, 'pickups': pickups})
 
 if __name__ == '__main__':
     app.run(debug=True)
